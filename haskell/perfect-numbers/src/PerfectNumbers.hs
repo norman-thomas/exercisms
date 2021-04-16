9f2c51bd0c377b516226cc3a0a8a74dbe0f7c399
Norman Thomas <news@hiroki.de>
2021-04-16 15:05:13 +0200

    simplify

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -4,12 +4,11 @@ data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
 classify n | n < 1             = Nothing
-           | aliquotSum n == n = Just Perfect
-           | aliquotSum n > n  = Just Abundant
+           | aliquotSum == n = Just Perfect
+           | aliquotSum > n  = Just Abundant
            | otherwise         = Just Deficient
+    where
+        aliquotSum = sum $ factors n
 
 factors :: Int -> [Int]
 factors n = [f | f <- [1..n `div` 2], n `mod` f == 0]
-
-aliquotSum :: Int -> Int
-aliquotSum = sum . factors
